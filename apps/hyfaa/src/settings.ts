d98d5429ba6ec00550bba8779af9eeda6214ca85
Florent gravin <florent.gravin@camptocamp.com>
2021-03-05 16:31:47 +0100

    chore: add plugin system

---
+++ apps/hyfaa/src/settings.ts
@@ -0,0 +1,16 @@
+import 'whatwg-fetch';
+
+class Settings {
+  apiUrl: '';
+
+  init() {
+    return fetch('assets/settings.json')
+      .then((response) => response.json())
+      .then((json) => {
+        Object.assign(this, json);
+      });
+  }
+}
+
+const SETTINGS = new Settings();
+export default SETTINGS;
