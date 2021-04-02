38772ce90930c3985b63dbee1cb166812874c56d
Florent gravin <florent.gravin@camptocamp.com>
2021-04-02 15:18:04 +0200

    refactor: add MapManager
    to centralize map mangement over components

--- apps/hyfaa/src/app/app.component.ts
+++ apps/hyfaa/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { FORMAT_BY_PROJECTION } from '@hydro-flow/ui/map'
-import Map from 'ol/Map'
-import View from 'ol/View'
-import TileLayer from 'ol/layer/Tile'
-import OSM from 'ol/source/OSM'
+import { MapManagerService } from './map/map-manager.service'
 
 @Component({
   selector: 'hyfaa-root',
@@ -12,22 +9,9 @@ import OSM from 'ol/source/OSM'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  map: Map
   mousePositionProj = 'EPSG:4326'
   formatByProjection = FORMAT_BY_PROJECTION
 
-  ngOnInit(): void {
-    this.map = new Map({
-      controls: [],
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: [0, 0],
-        zoom: 2,
-      }),
-    })
-  }
+  constructor(public mapManager: MapManagerService) {}
+  ngOnInit(): void {}
 }
--- apps/hyfaa/src/app/app.module.ts
+++ apps/hyfaa/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { UiMapModule } from '@hydro-flow/ui/map'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { MapContainerComponent } from './components/map-container/map-container.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MapContainerComponent],
   imports: [BrowserModule, UiMapModule],
   providers: [],
   bootstrap: [AppComponent],
