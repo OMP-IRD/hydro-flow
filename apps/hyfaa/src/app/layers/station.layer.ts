77a3c134eed80634c1f112890b4d9c58362de0f5
Florent gravin <florent.gravin@camptocamp.com>
2021-04-26 17:55:33 +0200

    feat: station layer hover cursor

--- apps/hyfaa/src/app/layers/station.layer.ts
+++ apps/hyfaa/src/app/layers/station.layer.ts
@@ -3,7 +3,9 @@ import { Extent } from 'ol/extent'
 import Feature from 'ol/Feature'
 import MVT from 'ol/format/MVT'
 import VectorTileLayer from 'ol/layer/VectorTile'
+import Map from 'ol/Map'
 import VectorTileSource from 'ol/source/VectorTile'
+
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
 import SETTINGS from '../../settings'
 import { MapManagerService } from '../map/map-manager.service'
@@ -14,10 +16,13 @@ export const STATION_COLOR = 'rgb(222, 43, 178)'
   providedIn: 'root',
 })
 export class StationLayer {
+  map: Map
   private layer: VectorTileLayer
   private source: VectorTileSource
 
   constructor(private mapManager: MapManagerService) {
+    this.map = this.mapManager.map
+
     this.source = new VectorTileSource({
       format: new MVT({
         featureClass: Feature,
@@ -30,6 +35,33 @@ export class StationLayer {
     this.layer = new VectorTileLayer({
       source: this.source,
       style: this.styleFn.bind(this),
+    })
+
+    this.initInteractions_()
+  }
+
+  private initInteractions_(): void {
+    this.map.on('pointermove', (event) => {
+      const hovering = this.map.forEachLayerAtPixel(
+        event.pixel,
+        (layer) => layer === this.getLayer()
+      )
+      if (hovering) {
+        this.map.getTarget().style.cursor = 'pointer'
+      } else {
+        this.map.getTarget().style.cursor = ''
+      }
+    })
+    this.map.on('click', (event) => {
+      const hit = this.map.forEachFeatureAtPixel(
+        event.pixel,
+        (feature: Feature) => {
+          return feature
+        },
+        { layerFilter: (layer) => layer === this.getLayer() }
+      )
+      if (hit) {
+      }
     })
   }
 
@@ -58,15 +90,6 @@ export class StationLayer {
           color: setRgbOpacity(color, 0.5),
         }),
       }),
-      /*
-      text: new Text({
-        text: size.toString(),
-        scale: 1.8,
-        fill: new Fill({
-          color: '#fff',
-        }),
-      }),
-*/
     })
   }
 }
