1a8b78c9e42613fc9b5669be2778af7eb3e07e2d
Florent gravin <florent.gravin@camptocamp.com>
2021-05-10 09:50:53 +0200

    fix: loadData cancel requests

--- libs/feature/hydro/src/lib/stations/+state/station-data/station-data.effects.ts
+++ libs/feature/hydro/src/lib/stations/+state/station-data/station-data.effects.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core'
 import { StationsApiService } from '@hydro-flow/data-access/hyfaa'
 import { Actions, createEffect, ofType } from '@ngrx/effects'
-import { fetch } from '@nrwl/angular'
-import { map } from 'rxjs/operators'
+import { of } from 'rxjs'
+import { catchError, map, switchMap } from 'rxjs/operators'
 import * as StationDataActions from './station-data.actions'
 
 @Injectable()
@@ -10,22 +10,17 @@ export class StationDataEffects {
   init$ = createEffect(() =>
     this.actions$.pipe(
       ofType(StationDataActions.loadStationData),
-      fetch({
-        run: ({ stationId, options }) => {
-          return this.stationsApi
-            .getStationData(options.dataSerie, stationId)
-            .pipe(
-              map((data) =>
-                StationDataActions.loadStationDataSuccess({ stationData: data })
-              )
-            )
-        },
-
-        onError: (action, error) => {
-          console.error('Error', error)
-          return StationDataActions.loadStationDataFailure({ error })
-        },
-      })
+      switchMap(({ stationId, options }) =>
+        this.stationsApi.getStationData(options.dataSerie, stationId)
+      ),
+      map((data) =>
+        StationDataActions.loadStationDataSuccess({
+          stationData: data,
+        })
+      ),
+      catchError((error) =>
+        of(StationDataActions.loadStationDataFailure({ error }))
+      )
     )
   )
 
