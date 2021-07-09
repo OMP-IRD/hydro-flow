f9765d0689d3463f48a99a292a4298f40b796e60
Florent gravin <florent.gravin@camptocamp.com>
2021-07-09 13:48:32 -0400

    hydroweb: handle TZ in date inputs

--- vendor/d3.hydroweb.js
+++ vendor/d3.hydroweb.js
@@ -87,13 +87,17 @@ var tickFormat = localeFormatter.timeFormat.multi([
 ])
 
 Date.prototype.yyyymmdd = function () {
-  var yyyy = this.getFullYear().toString()
-  var mm = (this.getMonth() + 1).toString() // getMonth() is zero-based
-  var dd = this.getDate().toString()
+  var date = this.toTZ()
+  var yyyy = date.getFullYear().toString()
+  var mm = (date.getMonth() + 1).toString() // getMonth() is zero-based
+  var dd = date.getDate().toString()
 
   return (
     yyyy + '-' + (mm[1] ? mm : '0' + mm[0]) + '-' + (dd[1] ? dd : '0' + dd[0])
   )
+}
+Date.prototype.toTZ = function () {
+  return new Date(this.getTime() + Math.abs(this.getTimezoneOffset() * 60000))
 }
 
 Date.prototype.yyyymmddhhmmss = function () {
