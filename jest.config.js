aef3ca16c936d5536b70ad156591db6a7f867f5e
Florent gravin <florent.gravin@camptocamp.com>
2022-06-20 09:49:23 +0200

    chore(jest): transformPatterns openlayers

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 const { getJestProjects } = require('@nrwl/jest')
 
-module.exports = { projects: getJestProjects() }
+module.exports = {
+  projects: [
+    ...getJestProjects(),
+    '<rootDir>/libs/ui',
+    '<rootDir>/libs/feature',
+    '<rootDir>/libs/data-access/hyfaa',
+  ],
+}
