80a4a4381d249e2265c4cf3403ce90210eb03284
Florent gravin <florent.gravin@camptocamp.com>
2022-11-20 22:06:12 +0100

    refactor(sagui): add hover stations effect

--- apps/sagui/src/app/layers/station.layer.ts
+++ apps/sagui/src/app/layers/station.layer.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core'
 import { StationsFacade } from '@hydro-flow/feature/hydro'
 import { DateFacade } from '@hydro-flow/feature/time'
+import { MapBrowserEvent } from 'ol'
 import { Extent } from 'ol/extent'
 import Feature from 'ol/Feature'
 import VectorLayer from 'ol/layer/Vector'
 import Map from 'ol/Map'
 import VectorSource from 'ol/source/Vector'
 import { Style } from 'ol/style'
-import { filter, skip } from 'rxjs/operators'
+import { from, fromEvent } from 'rxjs'
+import { debounceTime, filter, skip, switchMap } from 'rxjs/operators'
 import { SaguiFacade } from '../+state/sagui.facade'
 import { MapManagerService } from '../map/map-manager.service'
 import { stationStyleFn } from './station.style'
@@ -21,6 +23,7 @@ export class StationLayer {
   private layer: VectorLayer<VectorSource>
   private source: VectorSource
   private currentDate: string
+  private hoverStation: Feature = null
 
   constructor(
     private mapManager: MapManagerService,
@@ -58,15 +61,23 @@ export class StationLayer {
   }
 
   private initInteractions_(): void {
-    this.map.on('pointermove', (event) => {
-      const target = this.map.getTarget() as HTMLElement
-      const hovering = this.getHit(event.pixel)
-      if (hovering) {
-        target.style.cursor = 'pointer'
-      } else {
-        target.style.cursor = ''
-      }
-    })
+    fromEvent<MapBrowserEvent<any>>(this.map, 'pointermove')
+      .pipe(switchMap((event) => from(this.layer.getFeatures(event.pixel))))
+      .subscribe((features) => {
+        const target = this.map.getTarget() as HTMLElement
+        if (features.length > 0) {
+          target.style.cursor = 'pointer'
+          this.hoverStation = features[0]
+          this.layer.changed()
+        } else {
+          target.style.cursor = ''
+          if (this.hoverStation) {
+            this.hoverStation = null
+            this.layer.changed()
+          }
+        }
+      })
+
     this.map.on('click', (event) => {
       const hit = this.map.forEachFeatureAtPixel(
         event.pixel,
@@ -93,21 +104,14 @@ export class StationLayer {
     this.source.clear()
   }
 
-  stationStyleFn(feature: Feature, resolution: number): Style | Style[] {
+  stationStyleFn(feature: Feature): Style | Style[] {
     const level = feature
       .get('levels')
       .find((value) => value.date === this.currentDate)?.level
 
-    return stationStyleFn(level)
-  }
-
-  private getHit(pixel: number[]) {
-    return this.map.forEachFeatureAtPixel(
-      pixel,
-      (feature: Feature) => {
-        return feature
-      },
-      { layerFilter: (layer) => layer === this.getLayer() }
+    return stationStyleFn(
+      level,
+      !!(this.hoverStation && feature === this.hoverStation)
     )
   }
 }
--- apps/sagui/src/app/layers/station.style.ts
+++ apps/sagui/src/app/layers/station.style.ts
@@ -3,23 +3,37 @@ import { alertCodeToColor, alertCodeToIcon } from '../ui/ui.utils'
 
 export type LevelType = 'n' | 'd1' | 'd2' | 'd3' | 'f1' | 'f2' | 'f3'
 
-export function stationStyleFn(level: LevelType): Style[] {
+const stylesCache = {}
+
+export function stationStyleFn(level: LevelType, hover: boolean): Style[] {
   if (level === undefined) return
 
-  return [
-    new Style({
+  const colorCacheKey = (level[1] || '0') + (hover ? '_h' : '')
+  const iconCacheKey = level[0] + (hover ? '_h' : '')
+  const zIndex = hover ? 10 : 1
+  const radius = hover ? 24 : 20
+  const scale = hover ? 0.24 : 0.2
+
+  if (!stylesCache[colorCacheKey]) {
+    stylesCache[colorCacheKey] = new Style({
+      zIndex,
       image: new Circle({
-        radius: 20,
+        radius: hover ? 24 : 20,
         fill: new Fill({
           color: alertCodeToColor(level),
         }),
       }),
-    }),
-    new Style({
+    })
+  }
+  if (!stylesCache[iconCacheKey]) {
+    stylesCache[iconCacheKey] = new Style({
+      zIndex,
       image: new Icon({
         src: alertCodeToIcon(level),
-        scale: [0.2, 0.2],
+        scale: [scale, scale],
       }),
-    }),
-  ]
+    })
+  }
+
+  return [stylesCache[colorCacheKey], stylesCache[iconCacheKey]]
 }
